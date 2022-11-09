ac659c7dceabff8fdc1c19122a056c93731a3371
Jokūbas K <jokubaskr@gmail.com>
2022-11-09 17:55:26 +0200

    tasks written

--- 001_BasicMokymai/Program.cs
+++ 001_BasicMokymai/Program.cs
@@ -4,7 +4,98 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, aliens! 👽");
+            //            Console.WriteLine("Hello, aliens! Oooh-lala 👽");
+
+            //            Console.Write(" added a lil");
+            //            Console.Write(" something here ;)\n");
+
+            //            Console.Beep();
+
+            //            var compo = "output";
+
+            //            Console.WriteLine("composition {0} things", compo);
+
+            //            Console.WriteLine($"Interpolation {compo} things...");
+
+            //            Console.WriteLine("Col1\tCol2\tCol3");
+            //            Console.WriteLine("Row1\r\nRow2\r\nRow3\r\n");
+            //            Console.WriteLine($"Row 1{Environment.NewLine}Row 2{Environment.NewLine}Row 3");
+
+            //            Console.WriteLine("\"Very\" important text");
+
+            //            //Console.WriteLine("---- Any key to continue ----");
+            //            //Console.ReadKey();
+            //            //Console.Clear();
+
+            //            //Console.WriteLine("Your input:");
+            //            //Console.Write(Environment.NewLine);
+            //            //var input = Console.ReadKey();
+            //            //Console.WriteLine();
+            //            //Console.WriteLine(input);
+
+            //            //Console.WriteLine("Try again:");
+            //            //input = Console.ReadKey();
+            //            //Console.WriteLine();
+            //            //Console.WriteLine(input.KeyChar);
+            //            //Console.WriteLine(input.Key);
+
+            //            //Console.WriteLine("".PadRight(30, 'a'));
+
+            //            Console.WriteLine("I SHALL GUESS THE FIRST CHARACTER YOU ENTEEEER! 🧙‍");
+            //            var name = Console.ReadLine();
+            //            name ??= "";
+            //            var output = name[0];
+            //            if (name.Length <= 0)
+            //            {
+            //                output = '\u4566';
+            //            }
+            //            Console.WriteLine($"I IS.... ✨\"{output}\"!!!✨");
+            //            Console.WriteLine("Vee" +
+            //                "sadas" +
+            //                "deee");
+
+            //            Console.WriteLine(@"
+            //adsad
+            //sadas
+            //fff
+            //");
+
+
+            Console.WriteLine("Type in your name please:");
+            var name = Console.ReadLine();
+            Console.WriteLine();
+            Console.WriteLine($"Your name is: {name}");
+
+            if (name == null || name.Length == 0)
+            {
+                name = " ";
+            }
+            var code = (int)name[0];
+            Console.WriteLine($"First letter ASCII code is: {code}");
+
+            Console.WriteLine("Type in a number:");
+            var number = int.Parse(Console.ReadLine() ?? "0");
+            Console.WriteLine($"Number added to ASCII code: {code + number}");
+
+            Console.WriteLine($"(1) Buy {Environment.NewLine}(2) Sell {Environment.NewLine}(3) Inventory");
+            var key = Console.ReadKey();
+            Console.WriteLine();
+            switch (key.KeyChar)
+            {
+                case '1':
+                    Console.WriteLine("Buying...");
+                    break;
+                case '2':
+                    Console.WriteLine("Selling...");
+                    break;
+                case '3':
+                    Console.WriteLine("Inventory...");
+                    break;
+                default:
+                    Console.WriteLine("I'm not sure what you want to do.");
+                    break;
+            }
+
         }
     }
 }
